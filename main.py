86dbcf017b427eeb05b6f843ee6139322101a69f
Matěj Svoboda <svobodamatej97@gmail.com>
2024-01-22 21:03:47 +0100

    sending email now work

--- main.py
+++ main.py
@@ -8,15 +8,12 @@ my_email = "emailtest789456123@gmail.com"
 password = "wresvssvntmrgnut"
 day_of_week = dt.datetime.now().weekday()
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="emailtest789456123@yahoo.com",
-        msg="fSubject:Hello\n\n"
-    )
-
 if day_of_week == 0:
-    print(random.choice(quotes))
-
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs="emailtest789456123@yahoo.com",
+            msg=f"Subject:Monday Anxiety Dose\n\n{random.choice(quotes)}"
+        )
